5684566d412d0a2d7775ec11d5358f4daf40d366
Yash <maddysai9848@gmail.com>
2025-11-27 23:59:31 +0530

    test 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	base: '/cie/',
 	plugins: [tailwindcss(), sveltekit()],
 	test: {
 		expect: { requireAssertions: true },
